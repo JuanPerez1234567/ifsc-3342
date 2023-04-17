47951353eee53d6d3ea314982f3ad991b2c207bc
JuanPerez1234567 <japerez@ualr.edu>
2023-04-17 23:51:47 +0000

    slideshows

--- gto_final_project/js/app.js
+++ gto_final_project/js/app.js
@@ -36,3 +36,55 @@ for (let item of items) {
       item.addEventListener("click", toggleItem, false);
     }
   }
+
+
+
+  function openService(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+
+
+  let slideIndex = [1,1,1];
+  let slideId = ["mySlides1","mySlides2", "mySlides3"]
+  showSlides(1, 0);
+  showSlides(1, 1);
+  showSlides(1, 2);
+
+  function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+  }
+  
+  function showSlides(n, no) {
+    let i;
+    let x = document.getElementsByClassName(slideId[no]);
+
+    if (n > x.length) {
+      slideIndex[no] = 1
+    }    
+    if (n < 1) {
+      slideIndex[no] = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    x[slideIndex[no]-1].style.display = "block";  
+  }
