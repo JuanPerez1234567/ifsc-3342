f559edcbdfddd8f7b004a2f990e0608fb20f0578
JuanPerez1234567 <japerez@ualr.edu>
2023-04-20 06:23:59 +0000

    final project

--- gto_final_project/assets/js/app.js
+++ gto_final_project/assets/js/app.js
@@ -91,3 +91,21 @@ for (let item of items) {
 
   // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+
+
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+  }
+}
+/* Event listener */
+document.addEventListener("click", closeSubmenu, false);
